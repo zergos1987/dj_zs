07630552bf40cc62e76bb9a387503f0754222602
Mischenko Vasily <4630574@mail.ru>
2022-08-31 20:35:43 +0300

    31082022 - #1

--- backend/app/api/models.py
+++ backend/app/api/models.py
@@ -18,10 +18,13 @@ default_for_app_settings= {
 class app_settings(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="api_app_settings")
     json_data = JSONField(null=True, blank=True, default=default_for_app_settings)
+    created_at_datetime = models.DateTimeField(auto_now_add=True, blank=False)
+    updated_at_datetime = models.DateTimeField(auto_now=True, blank=False)
 
     class Meta:
         # app_label helps django to recognize your db
-		app_label = 'api'
+        app_label = 'api'
+        ordering = ('-created_at_datetime',)
 
     def save(self, *args, **kwargs):
         if self.user and self.json_data.get("app_user").get("username") == 'anonymous':
@@ -29,8 +32,18 @@ class app_settings(models.Model):
         super(app, self).save(*args, **kwargs)
 
     def __str__(self):
-        field_values = []
-        for field in self._meta.get_fields():
-            field_values.append(str(getattr(self, field.name, '')))
-        field_values = ['user', 'settings', ]
-        return ' '.join(field_values)
+        created_at_datetime = self.created_at_datetime
+        if self.created_at_datetime: created_at_datetime = self.created_at_datetime.strftime("%m.%d.%Y, %H:%M:%S")
+        updated_at_datetime = self.updated_at_datetime
+        if self.updated_at_datetime: updated_at_datetime = self.updated_at_datetime.strftime("%m.%d.%Y, %H:%M:%S")
+        json_data = "api_app_settings"
+        username = "anonymous"
+        if self.user: username = self.user.username
+
+        return (
+            '[ id ]: ' + str(self.id) + ' ' +
+            '[ username ]: ' + str(username) + ' ' +
+            '[ json_data ]: ' + str(json_data) + ' ' +
+            '[ created_at_datetime ]: ' + str(created_at_datetime) + ' ' +
+            '[ updated_at_datetime ]: ' + str(updated_at_datetime)
+        )
