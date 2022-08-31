2ca031633bbd63e22b37e496c4146ea3ac16e7ac
Mischenko Vasily <4630574@mail.ru>
2022-08-31 20:16:02 +0300

    31082022 - #1

--- backend/app/api/models.py
+++ backend/app/api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+
 
 # Create your models here.
+app_data_v1 = {
+    "version": 1
+}
+
+default_for_app_settings= {
+    "app_name": "api",
+    "app_user": {
+            "username": "anonymous"
+    },
+    "app_data": app_data_v1
+}
+
+class app_settings(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="api_app_settings")
+    json_data = JSONField(null=True, blank=True, default=default_for_app_settings)
+
+    class Meta:
+        # app_label helps django to recognize your db
+		app_label = 'api'
+
+    def save(self, *args, **kwargs):
+        if self.user and self.json_data.get("app_user").get("username") == 'anonymous':
+            self.json_data["app_user"]["username"] = self.user.username
+        super(app, self).save(*args, **kwargs)
+
+    def __str__(self):
+        field_values = []
+        for field in self._meta.get_fields():
+            field_values.append(str(getattr(self, field.name, '')))
+        field_values = ['user', 'settings', ]
+        return ' '.join(field_values)
--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+from api.models import app_settings
 
 # Create your views here.
 
@@ -14,7 +14,7 @@ def index(request):
         user_settings = app_settings.objects.filter(user__isnull=True).first()
 
     context = {
-		'app_settings': user_settings,
+		'app_settings': user_settings.json_data,
         'data': {'col': 'test'},
 	}
     template = 'api/index.html'
