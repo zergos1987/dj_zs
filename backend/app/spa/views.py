26b4e28ce8d050a2a77e027cd9f3015562948965
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-05 15:22:11 +0300

    Update views.py

--- backend/app/spa/views.py
+++ backend/app/spa/views.py
@@ -9,16 +9,21 @@ def index(request):
     if not app_settings.objects.first():
         record = app_settings.objects.create()
         record.save()
-
+	
     if request.user.is_authenticated:
         user_settings = app_settings.objects.filter(user=request.user).first()
+        if not user_settings:
+            record = app_settings.objects.create()
+            record.user = request.user
+            record.save()
+            user_settings = record
     else:
         user_settings = app_settings.objects.filter(user__isnull=True).first()
 
     context = {
-		'app_settings': user_settings.json_data,
+        'app_settings': user_settings.json_data,
         'data': {'col': 'test'},
-	}
+    }
     template = 'spa/index.html'
 
     return render(request, template, context)
