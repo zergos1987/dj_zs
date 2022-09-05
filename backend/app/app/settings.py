c1bb43973eaa20952e0d0ee21fa3c10a995c5a52
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-05 14:17:21 +0300

    Update settings.py

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -141,6 +141,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static/assets",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
