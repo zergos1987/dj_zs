ee8f184e53e509d148f58691259f10315c2a7e56
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-07 18:15:04 +0300

    Update urls.py

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.shortcuts import redirect
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
