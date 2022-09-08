becfdae1d27991ae3d36ab66455dd8c384b00511
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-08 14:46:32 +0300

    Update urls.py

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.contrib.staticfiles.views import serve
 
 favicon_view = RedirectView.as_view(url='/static/assets/images/favicon.ico', permanent=True)
 
@@ -30,4 +32,7 @@ urlpatterns = [
     re_path(r'^favicon\.ico$', favicon_view),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static("/media/", document_root=settings.MEDIA_ROOT) + [
+        re_path(r"^static/(?P<path>.*)$", serve),
+    ]
