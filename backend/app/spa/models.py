fa8b1c50872b7e480024b0c895040ee6cf442b9e
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-08 09:41:01 +0300

    Update models.py

--- backend/app/spa/models.py
+++ backend/app/spa/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
