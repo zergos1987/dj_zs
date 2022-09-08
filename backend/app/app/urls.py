864cdea2b02d3a371907b680fef4347ad9f0a2b9
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-08 13:22:50 +0300

    Update urls.py

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 
-favicon_view = RedirectView.as_view(url='/static/base/assets/images/favicon.ico', permanent=True)
+favicon_view = RedirectView.as_view(url='/static/assets/images/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
