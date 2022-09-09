0a9e7dc5478fb7a936b676730dd2deeaebfecd08
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-09 16:17:10 +0300

    Update django_rest_framework.py

--- backend/app/app/extra_settings/django_rest_framework.py
+++ backend/app/app/extra_settings/django_rest_framework.py
@@ -1,12 +1,29 @@
 # SETTINGS PLUGIN: Django-rest-framework
+from datetime import timedelta
+from decouple import config
+import warnings
+from django.core.management.utils import get_random_secret_key
 
-INSTALLED_APPS = ['rest_framework']
+
+INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+]
 
 """
 Throttling is similar to permissions, in that it determines if a request should be authorized.
 Throttles indicate a temporary state, and are used to control the rate of requests that clients can make to an API.
 """
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
@@ -18,4 +35,36 @@ REST_FRAMEWORK = {
         'anon': '15/minute',
         'user': '100/minute'
     },
+}
+
+SIMPLE_JWT_SIGNING_KEY = config('SIMPLE_JWT_SIGNING_KEY')
+
+if not SIMPLE_JWT_SIGNING_KEY:
+    warnings.warn("SIMPLE_JWT_SIGNING_KEY not configured, using a random temporary key.")
+    SIMPLE_JWT_SIGNING_KEY = get_random_secret_key()
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=30),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SIMPLE_JWT_SIGNING_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
