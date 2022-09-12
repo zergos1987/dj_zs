1e164ded8f613bd2f5d685b5dbfca38be7709b1f
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-12 18:35:19 +0300

    Update urls.py

--- backend/app/api/urls.py
+++ backend/app/api/urls.py
@@ -5,12 +5,19 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.shortcuts import redirect
 
+
+app_name = 'api'
+
+
+
+API_VERSION = 'v1'
 
 schema_view = get_schema_view(
    openapi.Info(
       title="DJ ZS API",
-      default_version='v1',
+      default_version=API_VERSION,
       description="API",
       contact=openapi.Contact(email="admin@dj_zs.com"),
    ),
@@ -18,21 +25,43 @@ schema_view = get_schema_view(
    public=False,
    permission_classes=(permissions.AllowAny,),
 )
+from django.utils.safestring import mark_safe
 
 
+class DJZJ_APIRootView(routers.APIRootView):
+    """
+    Controls appearance of the API root view
+    """
 
-router = routers.DefaultRouter()
-router.register('v1/user_profile', views.UserProfile, basename="user_profile")
-#router.register('user_profile_files', views.UserProfileFiles, basename="user_profile_files")
+    def get_view_name(self) -> str:
+        return "Api Root"
 
-app_name = 'api'
+    def get_view_description(self, html=False) -> str:
+        text = f'<a href="/api/{API_VERSION}/swagger/" rel="nofollow"><span style="color: #001fff; ">Swagger</span></a>'
+        text = text + '</br>' + f'<a href="/api/{API_VERSION}/redoc/" rel="nofollow"><span  style="color: #001fff; ">Redoc</span></a>'
+        if html:
+            return mark_safe(f"<p>{text}</p>")
+        else:
+            return text
+
+
+class ApiRouter(routers.DefaultRouter):
+    APIRootView = DJZJ_APIRootView
+
+router = ApiRouter()
+#router.get_api_root_view().cls.__doc__ = f"DJ ZS API {API_VERSION} http://localhost:8100/api/v1/swagger"
+router.register('users', views.UsersViewSet, basename="users")
+router.register('users_profile', views.UsersProfileViewSet, basename="users_profile")
+router.register('users_profile_files', views.UsersProfileFilesViewSet, basename="users_profile_files")
+router.register('users_groups', views.GroupsViewSet, basename="users_groups")
 
 urlpatterns = [
     re_path(r'test_view/', views.index, name='index'),
-    re_path(r'^v1/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('v1/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('v1/token/access/', TokenRefreshView.as_view(), name='token_get_access'),
-    path('v1/token/both/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    re_path(r'^', include(router.urls))
+    re_path(fr'^{API_VERSION}/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(f'{API_VERSION}/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(f'{API_VERSION}/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(f'{API_VERSION}/token/access/', TokenRefreshView.as_view(), name='token_get_access'),
+    path(f'{API_VERSION}/token/both/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', lambda request: redirect(f'{API_VERSION}/', permanent=True)),
+    re_path(fr'^{API_VERSION}/', include(router.urls))
 ]
