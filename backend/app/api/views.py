4a617d5c7264cd8606acb70cf4732cb74c0b5806
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-12 18:36:53 +0300

    Update views.py

--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -5,9 +5,24 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import viewsets
 
-from api.models import app_settings
+from api.serializers import (
+    UsersSerializer,
+    GroupsSerializer,
+    UsersProfileSerializer,
+    UsersProfileFilesSerializer,
+)
+from django.contrib.auth.models import (
+    User,
+    Group,
+)
+from api.models import (
+    app_settings,
+    UserProfile,
+    UserProfileFiles,
+)
 
 
 
@@ -37,14 +52,40 @@ def index(request, *args, **kwargs):
 
 
 
-class UserProfile(GenericViewSet, ListModelMixin):
+class UsersViewSet(viewsets.ModelViewSet):
     """
-    User Profile data
+    User's data
     """
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UsersSerializer
 
-    def list(self, request, *args, **kwargs):
-        return Response(
-            data={"id": request.GET.get("id")},
-            status=HTTP_200_OK
-        )
+
+
+class UsersProfileViewSet(viewsets.ModelViewSet):
+    """
+    User's profile data
+    """
+    permission_classes = [IsAdminUser]
+    queryset = UserProfile.objects.all().order_by('-created_at_datetime')
+    serializer_class = UsersProfileSerializer
+
+
+
+class UsersProfileFilesViewSet(viewsets.ModelViewSet):
+    """
+    User's profile files data
+    """
+    permission_classes = [IsAdminUser]
+    queryset = UserProfileFiles.objects.all().order_by('-created_at_datetime')
+    serializer_class = UsersProfileFilesSerializer
+
+
+
+class GroupsViewSet(viewsets.ModelViewSet):
+    """
+    Group's data
+    """
+    permission_classes = [IsAdminUser]
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupsSerializer
