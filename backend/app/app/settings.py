7ddfa16286aa6d5c790492183f204e60c1bcce4a
Mischenko Vasily <4630574@mail.ru>
2022-09-15 19:54:20 +0300

    dj_zs_update

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -290,16 +290,22 @@ INSTALLED_APPS += ['django_filters']
 INSTALLED_APPS += ['csp']
 MIDDLEWARE += ['csp.middleware.CSPMiddleware']
 if DEBUG:
-    CSP_WORKER_SRC = ("'self'", "'unsafe-inline'", 'http: blob:')
+    CSP_WORKER_SRC = ("'self'", "'unsafe-inline'", 'http: blob:', 'https: blob:')
     CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'")
     CSP_STYLE_SRC = (
         "'self'", 
         "'unsafe-inline'", 
         'fonts.googleapis.com',
     )
-    CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'",)
+    CSP_SCRIPT_SRC = (
+        "'self'",
+        "'unsafe-inline'",
+        "https://stackpath.bootstrapcdn.com",
+        "https://cdn.jsdelivr.net",
+        "https://code.jquery.com",
+    )
     CSP_FONT_SRC = ("'self'", 'fonts.gstatic.com')
-    CSP_IMG_SRC = ("'self'", "'unsafe-inline'", 'http: data:', 'https: data:')
+    CSP_IMG_SRC = ("'self'", "'unsafe-inline'", 'http: data:', 'http: data:', 'https: data:')
 else:
     CSP_WORKER_SRC = ("'self'", "'unsafe-inline'", 'https: blob:')
     CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'")
