0343de00362cd5cf825039399d57baa98054ea08
Mischenko Vasily <4630574@mail.ru>
2022-09-15 15:25:08 +0300

    dj_zs_update

--- backend/app/app_accounts/models.py
+++ backend/app/app_accounts/models.py
@@ -10,10 +10,10 @@ from utils.models import validator_file_size, validator_file_extension
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
-from users_app.managers import UserManager
+from app_accounts.managers import UserManager
 
 
 
--- backend/app/app_accounts/signals.py
+++ backend/app/app_accounts/signals.py
@@ -1,7 +1,7 @@
 from django.dispatch import receiver
-from djoser.signals import user_registered, user_updated
+from djoser.signals import user_registered
 
-from accounts.models import UserProfile
+from app_accounts.models import UserProfile
 
 
 @receiver(user_registered, dispatch_uid="create_userprofile")
