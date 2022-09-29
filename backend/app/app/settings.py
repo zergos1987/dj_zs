3a14c03b0f4465b513a876c1a0e4da0d396f0be8
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-29 17:45:35 +0300

    Update settings.py

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -291,8 +291,8 @@ INSTALLED_APPS += ['django_filters']
 INSTALLED_APPS += ['csp']
 MIDDLEWARE += ['csp.middleware.CSPMiddleware']
 if DEBUG:
-    CSP_WORKER_SRC = ("'self'", "'unsafe-inline'")
-    CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'", 'http: blob:', 'https: blob:')
+    CSP_WORKER_SRC = ("'self'", "'unsafe-inline'", 'http: blob:', 'https: blob:',)
+    CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'")
     CSP_STYLE_SRC = (
         "'self'", 
         "'unsafe-inline'", 
@@ -308,8 +308,8 @@ if DEBUG:
     CSP_FONT_SRC = ("'self'", 'fonts.gstatic.com')
     CSP_IMG_SRC = ("'self'", "'unsafe-inline'", 'http: data:',  'https: data:')
 else:
-    CSP_WORKER_SRC = ("'self'", "'unsafe-inline'")
-    CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'", 'https: blob:')
+    CSP_WORKER_SRC = ("'self'", "'unsafe-inline'", 'https: blob:',)
+    CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'")
     CSP_STYLE_SRC = (
         "'self'", 
         "'unsafe-inline'", 
