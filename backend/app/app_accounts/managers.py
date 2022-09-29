bbed184a17324bb1f2f36b13c26d9e116d77379c
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-29 17:22:19 +0300

    Update managers.py

--- backend/app/app_accounts/managers.py
+++ backend/app/app_accounts/managers.py
@@ -4,8 +4,7 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, username=None, email=None, phone=None, \
-                     password=None, **extra_fields):
+    def _create_user(self, username=None, email=None, phone=None, password=None, **extra_fields):
         """
         Creates and saves a User with the given email and password.
         """
@@ -48,6 +47,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(username=username, email=email, password=password, **extra_fields)
 
@@ -56,6 +56,8 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
 
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
