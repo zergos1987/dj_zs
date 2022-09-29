5ee4576b03335e71bcf1e97eb269f337c60f991b
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-09-29 17:16:19 +0300

    Update models.py

--- backend/app/app_accounts/models.py
+++ backend/app/app_accounts/models.py
@@ -22,16 +22,35 @@ from app_accounts.managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(_('username'), max_length=255, unique=True)
-    email = models.EmailField(_('email address'),\
-        null=True, blank=True, unique=True)
-    phone = models.CharField(_('phone number'), max_length=30,\
-        null=True, blank=True, unique=True)
-    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
-    is_active = models.BooleanField(_('active'), default=False)
-    is_staff = models.BooleanField(_('staff'), default=False)
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_only."
+        ),
+        validators=[UnicodeUsernameValidator()],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    email = models.EmailField(_('email address'), null=True, blank=True, unique=True)
+    phone = models.CharField(_('phone number'), max_length=30, null=True, blank=True, unique=True)
+    is_active = models.BooleanField(
+        _("active"), 
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as active."
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    is_staff = models.BooleanField(
+        _("staff status"), 
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
 
-    is_verified = models.BooleanField(_('verified'), default=False)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
 
     objects = UserManager()
 
