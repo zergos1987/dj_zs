df0dc37a7bd4a2db05381a45542decc32960d8a6
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-10-03 12:04:09 +0300

    Create csp.py

---
+++ backend/app/app/extra_settings/csp.py
@@ -0,0 +1,23 @@
+INSTALLED_APPS = ['csp']
+MIDDLEWARE = ['csp.middleware.CSPMiddleware']
+
+CSP_WORKER_SRC = ("'self'", "'unsafe-inline'", 'http: blob:', 'https: blob:',)
+CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'")
+CSP_STYLE_SRC = (
+    "'self'",
+    "'unsafe-inline'",
+    'fonts.googleapis.com',
+)
+CSP_SCRIPT_SRC = (
+    "'self'",
+    "'unsafe-inline'",
+    "https://stackpath.bootstrapcdn.com",
+    "https://cdn.jsdelivr.net",
+    "https://code.jquery.com",
+)
+CSP_FONT_SRC = ("'self'", 'fonts.gstatic.com')
+CSP_IMG_SRC = ("'self'", "'unsafe-inline'", 'http: data:',  'https: data:')
+
+CSP_INCLUDE_NONCE_IN = [
+    "script-src"
+]
