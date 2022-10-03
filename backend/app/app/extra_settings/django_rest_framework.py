1224768ecd8f0f743d0c66c478c1eb8148247c18
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-10-03 16:12:42 +0300

    Update django_rest_framework.py

--- backend/app/app/extra_settings/django_rest_framework.py
+++ backend/app/app/extra_settings/django_rest_framework.py
@@ -7,6 +7,7 @@ from django.core.management.utils import get_random_secret_key
 
 INSTALLED_APPS = [
     'rest_framework',
+    # 'rest_framework_simplejwt',
     # 'rest_framework_simplejwt.token_blacklist',
 ]
 
@@ -38,34 +39,8 @@ REST_FRAMEWORK = {
     },
 }
 
-# SIMPLE_JWT_SIGNING_KEY = config('SIMPLE_JWT_SIGNING_KEY')
-#
-# if not SIMPLE_JWT_SIGNING_KEY:
-#     warnings.warn("SIMPLE_JWT_SIGNING_KEY not configured, using a random temporary key.")
-#     SIMPLE_JWT_SIGNING_KEY = get_random_secret_key()
-
 SIMPLE_JWT = {
-    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    # 'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    # 'ROTATE_REFRESH_TOKENS': False,
-    # 'BLACKLIST_AFTER_ROTATION': True,
-    #
-    # 'ALGORITHM': 'HS256',
-    # 'SIGNING_KEY': SIMPLE_JWT_SIGNING_KEY,
-    # 'VERIFYING_KEY': None,
-    # 'AUDIENCE': None,
-    # 'ISSUER': None,
-
     'AUTH_HEADER_TYPES': ('JWT', 'Bearer',),
-    # 'USER_ID_FIELD': 'username',
-    # 'USER_ID_CLAIM': 'user_id',
-    #
-    # 'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    # 'TOKEN_TYPE_CLAIM': 'token_type',
-    #
-    # 'JTI_CLAIM': 'jti',
-    #
-    # 'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    # 'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    # 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
