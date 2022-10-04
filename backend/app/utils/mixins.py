55cb1fa14c65e151515dfdb11585fed4535c234c
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-10-04 13:17:45 +0300

    Update mixins.py

--- backend/app/utils/mixins.py
+++ backend/app/utils/mixins.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class TimestampMixin(models.Model):
-    created_at = models.DateTimeField(_('Created at'), null=True, blank=True, auto_now_add=True)
-    updated_at = models.DateTimeField(_('Updated at'), null=True, blank=True, auto_now=True)
+    created_at = models.DateTimeField(null=True, blank=True, auto_now_add=True, verbose_name=_("Record created at"))
+    updated_at = models.DateTimeField(null=True, blank=True, auto_now=True, verbose_name=_("Record updated at"))
 
     class Meta:
         abstract = True
