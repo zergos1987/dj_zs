4728e37115835ab729b699fb05b9c1b4d670ec3c
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-10-07 17:54:45 +0300

    Update models_file_permissions.py

--- backend/app/utils/models_file_permissions.py
+++ backend/app/utils/models_file_permissions.py
@@ -1,1 +1,3 @@
-1
+def CreatePathFor_upload_users_username_files(instance, filename):
+    file_path = instance.user_files.path.replace(filename, '') + '\\users\\' + instance.user_profile.user.username + '\\files\\' + filename
+    return file_path
