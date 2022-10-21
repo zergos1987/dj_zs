8ed04fdca7a013e5c128925741128bbdf3759c41
MVasily <85062042+zergos1987@users.noreply.github.com>
2022-10-21 16:43:48 +0300

    Update settings.py

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'app_accounts.middleware.CustomMiddleware',
 ]
 
 ############
